aa6493814e86675719e958d10014cc22f07bf933
VladimirVozniak <87637075+VladimirVozniak@users.noreply.github.com>
2021-07-26 16:58:35 +0300

    Create task-10.js

---
+++ task-10.js
@@ -0,0 +1,34 @@
+// В функцию передаем коллекцию и 2 значения. По результату
+// выполнения вывести true, если у всех объектов одно свойство
+// равно первому параметру, а другое свойство равно второму параметру.
+
+let obj = [{
+    name: 'Вова',
+    age: 22,
+    hometown: 'Шахты'
+}, {
+    name: 'Илья',
+    age: 21,
+    hometown: 'Шахты'
+}, {
+    name: 'Саша',
+    age: 30,
+    hometown: 'Шахты'
+}, {
+    name: 'Аня',
+    age: 33,
+    hometown: 'Шахты'
+}]
+
+qwe = (arr, value1, value2) => {
+    for (let elem of arr) {
+        if (elem.age < value1 &&
+            elem.hometown === value2) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+console.log(qwe(obj, 50, 'Шахты'))
