c6778757a54957e182b10ac20e4c73e34d0962ae
VladimirVozniak <87637075+VladimirVozniak@users.noreply.github.com>
2021-08-04 16:08:40 +0300

    Create 2task-27.js

---
+++ 2task-27.js
@@ -0,0 +1,9 @@
+// 27. Дана строка. Вставить после каждого символа пробел.
+
+let str='text education part 2'
+
+qwe=(str)=>{
+    return str.split(' ').join('').split('').join(' ')
+}
+
+console.log(qwe(str));
